5bdc2fbbbe590d80aef53261ec5a0234378904e9
Edi Carreras <edi@poquesoft.net>
2019-12-10 11:23:22 +0100

    After Day 10

--- adventofcode/src/main/java/net/poquesoft/adventofcode/Advent10.kt
+++ adventofcode/src/main/java/net/poquesoft/adventofcode/Advent10.kt
@@ -1,8 +1,152 @@
 package net.poquesoft.adventofcode
 
+import java.io.File
+import kotlin.math.atan2
+import kotlin.math.sqrt
+
+val origin = Coor(25,31)
+
 fun main() {
-    val test1 = IntCode(listOf(109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99)) //takes no input and produces a copy of itself as output.
-    test1.addZeroes()
-    test1.run()
-    println("Should have output (109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99)")
+    val test1 = processMap2("input10.txt")
+    println("")
+}
+
+
+fun processMap2(file: String) {
+    val asteroids = mutableListOf<Coor>()
+    var i = 0
+    File(file).forEachLine {
+        var j=0
+        it.map {it2->
+            if (it2 == '#') {
+                asteroids.add(Coor(j, i))
+            }
+            j++
+        }
+        i++
+    }
+
+    val control = Coor(14,31)
+    val remain = asteroids.toMutableList()
+    remain.remove(origin)
+    var previous = Coor(24,1)
+    for (i in 1..200) {
+        val next = nextToVaporize(remain, previous)
+        remain.remove(next)
+        println ("$i -> $next")
+        previous = next
+        if (previous == control)
+            println("CONTROL")
+    }
+}
+
+fun nextToVaporize(remain: MutableList<Coor>, p: Coor): Coor {
+    var next = Coor(0,0)
+    var previous = p
+    if (previous == Coor(14,31))
+        previous = Coor(0,30)
+    val d = previous - origin
+    val angle =atan2(d.y.toDouble(),d.x.toDouble())
+    val mod1= sqrt(d.x.toDouble()*d.x.toDouble() + d.y.toDouble()*d.y.toDouble())
+    var minAngle = 1000.0
+    remain.map{
+        val angle2 = atan2((it-origin).y.toDouble(),(it-origin).x.toDouble())
+        val mod2= sqrt((it-origin).x.toDouble()*(it-origin).x.toDouble() + (it-origin).y.toDouble()*(it-origin).y.toDouble())
+        if (angle2 > angle && (angle2-angle)<minAngle){
+            minAngle = angle2-angle
+            next = it
+        } else if ((angle2-angle) == minAngle && mod2<mod1) {
+            next = it
+        }
+    }
+    if (next == Coor(0,0)) println("ERROR")
+    return next
+}
+
+fun processMap1(file: String) {
+    val asteroids = mutableListOf<Coor>()
+    var i = 0
+    File(file).forEachLine {
+        var j=0
+        it.map {it2->
+            if (it2 == '#') {
+                asteroids.add(Coor(j, i))
+            }
+            j++
+        }
+        i++
+    }
+    var best = Coor(0,0)
+    var max = 0
+    asteroids.map {
+        var visible = asteroids.toMutableList()
+        val origin = Coor(it.x, it.y)
+        if (origin == Coor(3,4))
+            print ("")
+        visible.remove(origin)
+        asteroids.map { it2 ->
+            if (it2 != origin) {
+                val d = Coor(it2.x - origin.x, it2.y - origin.y)
+                asteroids.map { hidden ->
+                    if (hidden != it2 && hidden != origin) {
+                        val d2 = Coor(hidden.x - origin.x, hidden.y - origin.y)
+                        if (hides(d, d2))
+                            if (visible.contains(hidden)) visible.remove(hidden)
+                    }
+                }
+            }
+        }
+        if (visible.size > max) {
+            best = origin
+            max = visible.size
+        }
+        println ("$origin ${visible.size}")
+    }
+    println("")
+    println ("$best $max")
+}
+
+
+
+fun hides (d:Coor, d2:Coor): Boolean {
+    val angle1 =atan2(d.y.toDouble(),d.x.toDouble())
+    val angle2 =atan2(d2.y.toDouble(),d2.x.toDouble())
+    val mod1= sqrt(d.x.toDouble()*d.x.toDouble() + d.y.toDouble()*d.y.toDouble())
+    val mod2= sqrt(d2.x.toDouble()*d2.x.toDouble() + d2.y.toDouble()*d2.y.toDouble())
+    return (angle1 == angle2 && mod1 < mod2)
+}
+
+fun hides2(d: Coor, d2: Coor): Boolean {
+    if (d.x == 0 && d2.x == 0) return (d2.y > d.y && d2.y*d.y>0)
+    if (d.y == 0 && d2.y == 0) return (d2.x > d.x && d2.x*d.x>0)
+    if (d.x == 0 && d2.x != 0) return false
+    if (d.y == 0 && d2.y != 0) return false
+    if (d.x != 0 && d2.x == 0) return false
+    if (d.y != 0 && d2.y == 0) return false
+    if (d.x.toDouble()/d.y.toDouble() == d2.x.toDouble()/d2.y.toDouble() &&
+        (d2.x > d.x || d2.y > d.y)
+        && (d2.y*d.y>0)
+        && (d2.x*d.x>0)
+        )
+        return true
+    return false
+}
+
+private operator fun Coor.times(d: Coor): Int {
+    return x*x + y*y
+}
+
+private operator fun Coor.times(i: Int): Coor {
+    return Coor(x*i,y*i)
+}
+
+
+data class Coor(val x: Int, val y: Int) {
+    operator fun plus(d: Coor): Coor {
+        return Coor(x+d.x,y+d.y)
+    }
+
+    operator fun minus(d: Coor): Coor {
+        return Coor(x-d.x,y-d.y)
+    }
 }
